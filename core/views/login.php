13e783f23b85e21e181427d50f25942a4b7050fb
Ville Viklund <ville.viklund@gmail.com>
2013-10-08 22:04:48 +0300

    Added simple user authentication
    Added passphrase based authentication.

---
+++ core/views/login.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title>TurboCMS</title>
+    <link rel="stylesheet" type="text/css" href="core/css/turbocms.css"/>
+</head>
+<body>
+
+<div class="container">
+    <div class="header">
+        <h1>TurboCMS</h1>
+    </div>
+    <div class="content">
+        <form action="" method="post">
+            <div class="form-element">
+                <label for="passphrase">Passphrase:</label>
+                <input type="password" name="passphrase" id="passphrase"/>
+            </div>
+            <div class="form-element">
+                <input type="submit" value="Login" class="form-submit" />
+            </div>
+        </form>
+    </div>
+</div>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 require 'vendor/autoload.php';
 require 'config.php';
 require 'core/Parser.php';
@@ -14,7 +16,6 @@ $app = new \Slim\Slim(
  * Frontpage
  */
 $app->get('/', function () use($app) {
-
     if (file_exists($app->config('templates.path') . '/index.html')) {
         $app->render('index.html');
     } else {
@@ -26,6 +27,10 @@ $app->get('/', function () use($app) {
  * Admin form
  */
 $app->get('/admin', function () use ($app, $settings) {
+    if (!password_verify($settings['passphrase'], $_SESSION['turbo_cms_login'])) {
+        $app->response->redirect('login');
+    }
+
     $parser = new \TurboCMS\Parser($settings);
     $layoutKeys = $parser->getKeys();
     $app->render('admin_form.php', array('keys' => $layoutKeys));
@@ -35,11 +40,34 @@ $app->get('/admin', function () use ($app, $settings) {
  * Handle admin form submission
  */
 $app->post('/admin', function () use ($app, $settings) {
+    if (!password_verify($settings['passphrase'], $_SESSION['turbo_cms_login'])) {
+        $app->response->redirect('login');
+    }
+
     $postVars = $app->request->post();
     $parser = new \TurboCMS\Parser($settings);
     $parser->createFile($postVars);
 
     $app->response->redirect($app->urlFor('frontpage'));
+});
+
+/**
+ * Login form
+ */
+$app->get('/login', function () use ($app) {
+    $app->render('login.php');
+});
+
+/**
+ * Handle login form submission
+ */
+$app->post('/login', function () use ($app, $settings) {
+    if ($app->request()->post('passphrase') == $settings['passphrase']) {
+        $_SESSION['turbo_cms_login'] = password_hash($settings['passphrase'], PASSWORD_BCRYPT);
+        $app->response->redirect('admin');
+    } else {
+        $app->render('login.php');
+    }
 });
 
 $app->run();
